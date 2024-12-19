43c4ee4e7a0ab6c1aa35b0cc9e20dbd991391546
friday-ugbebor <ugbeboor18@yahoo.com>
2024-12-18 22:22:21 -0600

    Terraform setup and workflow

--- backend.tf
+++ backend.tf
@@ -1,9 +1,19 @@
 terraform {
   backend "s3" {
-    bucket         = "element84-terraform-state"  # S3 bucket for Terraform state
-    key            = "state/app.tfstate"         # Path to the state file
-    region         = "us-east-1"                 # AWS region
-    dynamodb_table = "element84-terraform-locks" # DynamoDB table for state locking
-    encrypt        = true                        # Encrypt the state file
+    bucket         = var.backend_bucket_name
+    key            = "state/app.tfstate"
+    region         = var.aws_region
+    dynamodb_table = var.dynamodb_table_name
+    encrypt        = true
   }
 }
+
+# terraform {
+#   backend "s3" {
+#     bucket         = "element84-terraform-state"  # S3 bucket for Terraform state
+#     key            = "state/app.tfstate"         # Path to the state file
+#     region         = "us-east-1"                 # AWS region
+#     dynamodb_table = "element84-terraform-locks" # DynamoDB table for state locking
+#     encrypt        = true                        # Encrypt the state file
+#   }
+# }
--- variables.tf
+++ variables.tf
@@ -1,25 +1,25 @@
-variable "s3_bucket_name" {
-  description = "Name of the S3 bucket for Terraform state"
+variable "backend_bucket_name" {
+  description = "S3 bucket name for storing Terraform state"
   type        = string
-  default     = "element84-terraform-state"
+  default     = "element84-terraform-state" 
 }
 
 variable "dynamodb_table_name" {
-  description = "Name of the DynamoDB table for Terraform state locking"
+  description = "DynamoDB table name for state locking"
   type        = string
-  default     = "element84-terraform-locks"
-}
-
-variable "datasets_bucket_name" {
-  description = "Name of the S3 bucket for storing datasets"
-  type        = string
-  default     = "element84-datasets"
+  default     = "element84-terraform-locks" 
 }
 
 variable "aws_region" {
   description = "AWS region for all resources"
   type        = string
-  default     = "us-east-1"
+  default     = "us-east-1" 
+}
+
+variable "datasets_bucket_name" {
+  description = "S3 bucket name for storing datasets"
+  type        = string
+  default     = "element84-datasets" 
 }
 
 variable "lambda_function_name" {
@@ -32,4 +32,16 @@ variable "api_gateway_name" {
   description = "Name of the API Gateway"
   type        = string
   default     = "HomelessDataAPI"
+}
+
+variable "s3_raw_data_path" {
+  description = "Path in the S3 bucket for raw data files"
+  type        = string
+  default     = "raw/"
+}
+
+variable "s3_processed_data_path" {
+  description = "Path in the S3 bucket for processed data files"
+  type        = string
+  default     = "processed/"
 }
